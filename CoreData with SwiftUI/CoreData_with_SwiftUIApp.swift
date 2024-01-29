8892acac0eaf133c4220ecf7beb6ea0bd016f851
Cong Le <longchike@gmail.com>
2024-01-29 09:52:50 -0500

    initial setup

---
+++ CoreData with SwiftUI/CoreData_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_with_SwiftUIApp.swift
+//  CoreData with SwiftUI
+//
+//  Created by Cong Le on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
